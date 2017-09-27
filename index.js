9b8aad0c28b4d31ca4f7b472a3b11f178d94747c
Nathan Campbell <nathancampbell@outlook.com>
2017-09-27 18:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost() {
-  var pageTemplate = .template(document.getElementById("page-template").innerHTML);
-  var postTemplate = .template(document.getElementById("post-template").innerHTML);
-  var commentsTemplate = .template(document.getElementById("comments-template").innerHTML);
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById("postAuthor").value;
